fa4acae48b617f5b8abb6da0a93b8fba933a9b7b
shubhankit18 <56321296+shubhankit18@users.noreply.github.com>
2023-02-27 02:09:51 +0530

    Add files via upload
    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spotify Clone with HTML & CSS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/omerko96/pen/wvXgQZK](https://codepen.io/omerko96/pen/wvXgQZK).
+
+Video - https://youtu.be/xVZqEmRQjLM
---
+++ script.js
@@ -0,0 +1,100 @@
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let items = Array.from(document.getElementsByClassName('item'));
+
+let songs = [
+    {songName: "Kesariya From Brahmastra", filePath: "songs/1.mp3"},
+    {songName: "Ranjha from Shershaah", filePath: "songs/2.mp3", },
+    {songName: "Atak Gaya - Arijit Singh", filePath: "songs/3.mp3", },
+    {songName: "Kabira", filePath: "songs/4.mp3", },
+    {songName: "NO CHINA", filePath: "songs/5.mp3", },
+    {songName: "KALESHI CHORI", filePath: "songs/6.mp3", },
+    {songName: "Maan Meri Jaan", filePath: "songs/7.mp3", },
+    {songName: "Brown Munde", filePath: "songs/8.mp3", },
+    {songName: "Fair Trade (with Travis Scott)", filePath: "songs/9.mp3", },
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3",},
+]
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle"');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})  
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=15){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
